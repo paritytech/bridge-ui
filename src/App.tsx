3a156a13dc39b0e6dd54c14d1bc18769e16f6e01
thib <thibaut.sardan@gmail.com>
2020-08-31 15:54:32 +0200

    hardcoded transfer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
 // Copyright 2019-2020 @paritytech/bridge-ui authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-import * as React from 'react';
+import { ethers } from 'ethers';
+import React, { useEffect,useMemo,useState } from 'react';
 
-interface Props {
-   name:
-    string
+// extend the existing window interface to tell it about your new ethereum property.
+declare global {
+    interface Window { ethereum: any; }
 }
 
-class App extends React.Component<Props> {
-	render() {
-		const { name } = this.props;
-		return (
-			<>
-				<h1>
-          Hello {name}
-				</h1>
-			</>
-		);
-	}
-}
+window.ethereum = window.ethereum || {};
+
+const App = () => {
+	// A Web3Provider wraps a standard Web3 provider, which is
+	// what Metamask injects as window.ethereum into each page
+	const provider = useMemo(() => new ethers.providers.Web3Provider(window.ethereum), []);
+
+	// The Metamask plugin also allows signing transactions to
+	// send ether and pay to change state within the blockchain.
+	// For this, we need the account signer...
+	window.ethereum.enable();
+	const signer = provider.getSigner();
+	const [blockNumber, setBlockNumber] = useState(0);
+	const [accountList, setAccountList] = useState<string[]>([]);
+
+	console.log('accountList',accountList);
+	const sendTx = () => signer.sendTransaction({
+		// AccounId to hex from 5Ev8deqBc5bXB2pq2C9RWCBXM1kuS6wjqbZJiSRTA8kLZfTu
+		data: '0x7e2ae4cfcf198ca3bb3312e6409c65e87294c67a73d9cb00bd72e8deca626347',
+		to: '0xDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEF',
+		value: ethers.utils.parseEther('1.0')
+	})
+		.then(response => console.log('hash', response.hash))
+		.catch( e => console.error('Tx send error',e));
+
+	useEffect(() => {
+		provider.on('block', (blockNumber: number) => setBlockNumber(blockNumber));
+	}, [provider]);
+
+	useEffect(() => {
+		provider.listAccounts().then( a => setAccountList(a));
+	}, [provider]);
+
+	return (
+		<>
+			<h1>
+				block: {blockNumber}<br/>
+				accounts: {accountList.toString()}
+			</h1>
+			<button onClick={sendTx}>Send 1 ETH</button>
+		</>
+	);
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -7,4 +7,4 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 
 const mountNode = document.getElementById('app');
-ReactDOM.render(<App name="Jane" />, mountNode);
+ReactDOM.render(<App/>, mountNode);
